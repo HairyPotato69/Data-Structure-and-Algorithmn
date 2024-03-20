f1be94f6227472601b5083d20bbd490acf5613ca
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-20 22:54:47 +0800

    Update node.h

--- Linked List/Singly/node.h
+++ Linked List/Singly/node.h
@@ -1,1 +1,19 @@
+#pragma once
+#ifndef node_type
+#define node_type
 
+using type = int;
+
+class Node {
+	private:
+		type data{};
+		Node* next{};
+	public:
+		Node(const type data);
+		type get_data();
+		Node* get_next();
+		void set_next(Node* next);
+		void set_data(const type data);
+	};
+
+#endif
