b8e8a53ba1af07ea7550b87764e5acc36c20c2e0
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-20 22:58:52 +0800

    Update node.h

--- Linked List/Doubly/node.h
+++ Linked List/Doubly/node.h
@@ -1,1 +1,22 @@
+#pragma once
+#ifndef node_type
+#define node_type
 
+using type = int;
+
+	class Node {
+	private:
+		type data{};
+		Node* next{};
+		Node* previous{};
+	public:
+		Node(const type data);
+		type get_data();
+		Node* get_next();
+		Node* get_prev();
+		void set_next(Node* next);
+		void set_prev(Node* prev);
+		void set_data(const type data);
+	};
+
+#endif // node_type
