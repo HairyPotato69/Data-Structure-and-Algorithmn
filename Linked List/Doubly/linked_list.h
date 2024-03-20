14453801060c882c127e9b07fe32266a39fb0486
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-20 22:57:57 +0800

    Update linked_list.h

--- Linked List/Doubly/linked_list.h
+++ Linked List/Doubly/linked_list.h
@@ -1,1 +1,36 @@
+#pragma once
+#ifndef linked_list_type
+#define linked_list_type
+#include "node.h"
+#include <cstdlib> // #include <stdlib.h>
 
+class Linked_List{
+	private:
+		Node* head{};
+		Node* tail{};
+		int size{};
+	public:
+		Linked_List();
+		int get_size();
+		bool isEmpty();
+		bool isUpperLower(int& index);
+		Node* find_node(int index);
+
+		// insert node
+		void push(doubly::Node* new_node);
+		void push_back(doubly::Node* new_node);
+		bool insert(int index, const type data);
+		bool insert(const type data);
+		// get data
+		void front(type& value);
+		void back(type& value);
+		bool value_at(int index, type& value);
+		// delete node
+		void pop_front();
+		void pop_back();
+		bool remove(int index);
+		// print
+		void print();
+	};
+
+#endif // linked_list_type
