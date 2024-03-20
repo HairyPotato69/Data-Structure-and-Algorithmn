9b1e36ace5a1db3552a6669a727b96870a3208ed
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-20 22:53:07 +0800

    Update linked_list.h

--- Linked List/Singly/linked_list.h
+++ Linked List/Singly/linked_list.h
@@ -1,1 +1,34 @@
+#pragma once
+#ifndef linked_list_type
+#define linked_list_type
+#include "node.h"
+#include <cstdlib> // #include <stdlib.h>
 
+	class Linked_List {
+	private:
+		Node* head{};
+		Node* tail{};
+		int size{};
+	public:
+
+		Linked_List();
+		int get_size();
+		bool isEmpty();
+		Node* find_node(const int index);
+		void push(Node* new_node);
+		void push_back(Node* new_node);
+		bool insert(int index, const type data);
+		bool insert(const type data);
+		// get data
+		void front(type& value);
+		void back(type& value);
+		bool value_at(const int index, type& value);
+		// delete node
+		void pop_front();
+		//void pop_back();
+		bool remove(const int index);
+		// print
+		void print();
+	};
+
+#endif // linked_list_type
