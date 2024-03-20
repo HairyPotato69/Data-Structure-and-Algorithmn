acbf9603b59ad06e3047e17adb7b498cad91cb44
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-20 22:58:25 +0800

    Update node.cpp

--- Linked List/Doubly/node.cpp
+++ Linked List/Doubly/node.cpp
@@ -1,1 +1,32 @@
+#include "node.h"
+#include <cstdlib>
 
+Node::Node(const type data) {
+	this ->data = data;
+	next = NULL;
+	previous = NULL;
+}
+
+type Node::get_data() {
+	return data;
+}
+
+Node* Node::get_next() {
+	return next;
+}
+
+Node* Node::get_prev() {
+	return previous;
+}
+
+void Node::set_data(const type data) {
+	this->data = data;
+}
+void Node::set_next(Node* next) {
+	this->next = next;
+}
+
+void Node::set_prev(Node* prev)
+{
+	this->previous = prev;
+}
