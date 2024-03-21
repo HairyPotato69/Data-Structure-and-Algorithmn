5a109b537098f8d298687a8ddc9e74f11b83abc9
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-21 10:12:46 +0800

    Update queue.cpp

--- Linked List/Queue/queue.cpp
+++ Linked List/Queue/queue.cpp
@@ -1,6 +1,19 @@
 #include "queue.h"
 #include <iostream>
 
-int queue::getSize(){
+int Queue::getSize(){
   return size;
+}
+
+bool Queue::isEmpty(){
+  if (size == 0)
+    return 1;
+  else
+    return 0;
+}
+
+bool Queue::dequeue(type data){
+  Node* current_node{head};
+  head = current_node ->get_next();
+
 }
