1d329c6bb1b7fffce2f2294ff6820ede39e4a4ed
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-21 10:07:57 +0800

    Update queue.h

--- Linked List/Queue/queue.h
+++ Linked List/Queue/queue.h
@@ -1,1 +1,17 @@
+#ifndef queue_type
+#define queue_type 
 
+#include "node.h"
+
+
+class queue{
+    private:
+        int size{};
+        Node* head{};
+    public:
+        bool dequeue(type data);
+        bool enqueue();
+        bool isEmpty();
+        int getSize();
+};
+#endif
