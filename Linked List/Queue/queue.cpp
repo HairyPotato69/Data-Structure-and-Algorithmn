1a5f577d8e8310eecc43db38ccb4a2a29b49cf7a
HairyPotato69 <105530183+HairyPotato69@users.noreply.github.com>
2024-03-22 22:53:52 +0800

    Update queue.cpp

--- Linked List/Queue/queue.cpp
+++ Linked List/Queue/queue.cpp
@@ -1,23 +1,68 @@
 #include "queue.hpp"
 #include <iostream>
 
-int Queue::getSize(){
-  return size;
+linked_list::queue::queue::queue() {
+	head = NULL;
 }
 
-bool Queue::isEmpty(){
-  if (size == 0)
-    return 1;
-  else
-    return 0;
+bool linked_list::queue::queue::dequeue(type& data) {
+	
+	if (isEmpty())
+		return 0;
+	single_node::Node* first_node{ head };
+
+	if (get_size() == 1) {
+		data = first_node->get_data();
+		head = NULL;
+		delete first_node;
+		return 1;
+	}
+
+	data = first_node->get_data();
+	head = first_node->get_next();
+
+	delete first_node;
+	size--;
+	
+	return 1;
 }
 
-bool Queue::dequeue(type data){
-  if (isEmpty())
-    return 0;
-  Node* current_node{head};
-  head = current_node->get_next();
-  data = current_node->get_data();
-  delete current_node;
-  return 1;
+bool linked_list::queue::queue::enqueue(const type data) {
+	single_node::Node* new_node(new single_node::Node(data));
+	single_node::Node* current_node{ nullptr };
+	if (!new_node)
+		return 0;
+
+	if (isEmpty()) {
+		head = new_node;
+		size++;
+		return 1;
+	}
+
+	for (int i{ 1 }; i < size; i++)
+		current_node = current_node->get_next();
+	
+	current_node->set_next(new_node);
+	size++;
+	
+	return 1;
+}
+
+bool linked_list::queue::queue::isEmpty() {
+	if (size == 0)
+		return 1;
+	return 0;
+}
+
+int linked_list::queue::queue::get_size() {
+	return size;
+}
+
+bool linked_list::queue::queue::peek(type& data) {
+	if (isEmpty())
+		return 0;
+
+	data = head->get_data();
+	
+	return 1;
 }
